00b89573acd8e7afd2127ff47d681627d4dda084
zhang-xiang-zx <1915623562@qq.com>
2024-09-12 16:11:22 +0800

    添加springboot依赖和启动类

---
+++ src/main/java/cn/xiangstudy/Application.java
@@ -0,0 +1,15 @@
+package cn.xiangstudy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxiang
+ * @date 2024-09-12 16:05
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
+
